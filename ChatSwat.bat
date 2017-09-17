8ff4cbde6a4f8e6d2f8bc5e06a956868b55091ce
Abrar Ahmed <abrar.m.ahmed@gmail.com>
2017-09-17 12:58:35 -0400

    Create ChatSwat.bat

---
+++ ChatSwat.bat
@@ -0,0 +1,32 @@
+@echo off
+@echo begin...> logs.txt
+
+
+curl -X GET "https://api.groupme.com/v3/groups?token=YOURTOKEN&&per_page=500" -k > dump.txt
+set /p ans=<dump.txt
+
+findstr CHATNAME dump.txt >nul 2>&1
+IF not errorlevel 1 (
+   @echo chat Found>> logs.txt
+   python Scanner.py
+   set /p groupid=<codes.txt
+)ELSE (@echo Chat Not found.
+exit
+)
+
+:loop
+curl -X GET https://api.groupme.com/v3/groups/%groupid%?token=YOURTOKEN -k > dump.txt
+set /p ans=<dump.txt
+
+findstr Adam temp.txt >nul 2>&1
+IF not errorlevel 1 (
+   @echo Adam Found>> logs.txt
+   python Scanner.py
+   exit
+color 0A	
+)ELSE (@echo Adam Not found.
+color 0C
+)
+
+TIMEOUT 2
+goto loop
