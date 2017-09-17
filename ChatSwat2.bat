1a9ad1e328287c8c78da2aeffc29b9e3feeb16f4
Abrar Ahmed <abrar.m.ahmed@gmail.com>
2017-09-17 13:07:44 -0400

    Create ChatSwat2.bat

---
+++ ChatSwat2.bat
@@ -0,0 +1,17 @@
+:loop
+curl -X GET "https://api.groupme.com/v3/groups?token=YOURTOKEN&&per_page=500" -k > dump.txt
+set /p ans=<dump.txt
+
+findstr TARGETNAME dump.txt >nul 2>&1
+IF not errorlevel 1 (
+set /p var1= <codes.txt
+setlocal ENABLEDELAYEDEXPANSION
+for /F "skip=1" %%G in (codes.txt) do (
+    curl -X POST "https://api.groupme.com/v3/groups/%var1%/members/%%G/remove?token=YOURTOKEN" -k
+)
+color 0A
+	
+)ELSE (@echo Not found.
+)
+TIMEOUT 2
+goto loop
