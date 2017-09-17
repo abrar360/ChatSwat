6bd159226b03e4f11c5232c1f2727507a7a98c04
Abrar Ahmed <abrar.m.ahmed@gmail.com>
2017-09-17 13:12:25 -0400

    Update ChatSwat2.bat

--- ChatSwat2.bat
+++ ChatSwat2.bat
@@ -1,15 +1,16 @@
 @echo off
 @echo begin...> logs.txt
 :loop
+
+set /p groupid=<codes.txt
 curl -X GET https://api.groupme.com/v3/groups/%groupid%?token=YOURTOKEN -k > dump.txt
 set /p ans=<dump.txt
 
-findstr TARGETNAME dump.txt >nul 2>&1
+findstr Adam dump.txt >nul 2>&1
 IF not errorlevel 1 (
-set /p var1= <codes.txt
 setlocal ENABLEDELAYEDEXPANSION
 for /F "skip=1" %%G in (codes.txt) do (
-    curl -X POST "https://api.groupme.com/v3/groups/%var1%/members/%%G/remove?token=YOURTOKEN" -k
+    curl -X POST "https://api.groupme.com/v3/groupid/%var1%/members/%%G/remove?token=YOURTOKEN" -k
 )
 color 0A
 	
