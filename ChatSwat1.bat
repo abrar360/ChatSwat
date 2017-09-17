15090220cb0fda6b2620586652b246dc8e46dbac
Abrar Ahmed <abrar.m.ahmed@gmail.com>
2017-09-17 13:09:39 -0400

    Update ChatSwat1.bat

--- ChatSwat1.bat
+++ ChatSwat1.bat
@@ -18,7 +18,7 @@ exit
 curl -X GET https://api.groupme.com/v3/groups/%groupid%?token=YOURTOKEN -k > dump.txt
 set /p ans=<dump.txt
 
-findstr Adam temp.txt >nul 2>&1
+findstr Adam dump.txt >nul 2>&1
 IF not errorlevel 1 (
    @echo Adam Found>> logs.txt
    python Scanner.py
